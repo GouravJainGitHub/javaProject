f3584eb9f6c9d5bf293ac2df677f66893694cd79
goura <gouravj62@gmail.com>
2022-07-09 18:53:53 +0530

    HelloWord

---
+++ SET7/Helloword.java
@@ -0,0 +1,11 @@
+package SET7;
+
+public class Helloword {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        int a = 5;
+        int b = 6;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
