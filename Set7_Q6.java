3b0c126fb4ef7e85ace6b20afccfc6bef5fda3bb
goura <gouravj62@gmail.com>
2022-07-10 11:29:11 +0530

    Set_Q6

---
+++ Set7_Q6.java
@@ -0,0 +1,11 @@
+public class Set7_Q6 {
+    public static void main(String args[]) {
+        // Write a program to find sum up to n
+        int n = 10;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = i + n;
+            System.out.println(sum);
+        }
+    }
+}
