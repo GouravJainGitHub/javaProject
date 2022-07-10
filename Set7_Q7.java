64d539495532ad277eb2e98e01130bea1def2179
goura <gouravj62@gmail.com>
2022-07-10 11:38:33 +0530

    Set7_Q7

---
+++ Set7_Q7.java
@@ -0,0 +1,11 @@
+public class Set7_Q7 {
+    public static void main(String args[]) {
+        // Write a program to find the factorial of a number.
+        int F = 5;
+        int fact = 1;
+        for (int i = 1; i <= F; i++) {
+            fact = i * fact;
+            System.out.println(fact);
+        }
+    }
+}
