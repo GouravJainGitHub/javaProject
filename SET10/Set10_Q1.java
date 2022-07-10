904a6defd392c2658721086d2ac9953ef9a15344
goura <gouravj62@gmail.com>
2022-07-10 14:00:50 +0530

    P

--- SET10/Set10_Q1.java
+++ SET10/Set10_Q1.java
@@ -2,10 +2,14 @@ package SET10;
 
 public class Set10_Q1 {
     public static void main(String args[]) {
-        int i = 0;
-        while (i < 10) {
-            System.out.println(i);
-            i = i + 1;
+        // Write a program to find factorial of a number.
+        int n = 5;
+        int i = 1;
+        int sum = 1;
+        while (i <= n) {
+            sum = sum * i;
+            System.out.println(sum);
+            i++;
         }
     }
 }
---
+++ SET10/Set10_Q2.java
@@ -0,0 +1,7 @@
+package SET10;
+
+public class Set10_Q2 {
+    public static void main(String args[]) {
+
+    }
+}
