96199e59204406a3fc32c4aa76b7bbe8e68a908c
goura <gouravj62@gmail.com>
2022-07-10 14:06:55 +0530

    Set10_Q2

--- SET10/Set10_Q2.java
+++ SET10/Set10_Q2.java
@@ -2,6 +2,14 @@ package SET10;
 
 public class Set10_Q2 {
     public static void main(String args[]) {
-
+        // Write a program to find the sum of digits of given number.
+        int n = 6;
+        int i = 0;
+        int sum = 0;
+        while (i < n) {
+            sum = sum + i;
+            System.out.println(sum);
+            i++;
+        }
     }
 }
