1913ea895c9415bac9fbbcffc31d716a285064b6
goura <gouravj62@gmail.com>
2022-07-10 10:38:54 +0530

    New Program

--- SET7/Set7_Q1.java
+++ SET7/Set7_Q1.java
@@ -4,7 +4,7 @@ package SET7;
 public class Set7_Q1 {
     public static void main(String args[]) {
         int num = 0;
-        for (int i = 0; i <= 100; i++) {
+        for (int i = num; i <= 100; i++) {
             System.out.println(i);
         }
     }
