d36935092ebf52d2cdd2dde777b215f2f0ed36b8
goura <gouravj62@gmail.com>
2022-07-10 10:20:34 +0530

    Program

---
+++ SET7/Set7_Q1.java
@@ -0,0 +1,9 @@
+package SET7;
+
+// Write a program to Print number from 1 to n.
+public class Set7_Q1 {
+    public static void main(String args[]) {
+        Integer num = 0;
+        System.out.println(num);
+    }
+}
