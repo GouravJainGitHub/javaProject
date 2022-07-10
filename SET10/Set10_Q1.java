a2cf8b5148016846f142a292392bfbb14fd84520
goura <gouravj62@gmail.com>
2022-07-10 13:35:33 +0530

    Latest

---
+++ SET10/Set10_Q1.java
@@ -0,0 +1,11 @@
+package SET10;
+
+public class Set10_Q1 {
+    public static void main(String args[]) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i = i + 1;
+        }
+    }
+}
