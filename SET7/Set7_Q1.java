e671c3d5d0fa205b90cc0c0370c28de881f7430d
goura <gouravj62@gmail.com>
2022-07-10 10:24:14 +0530

    Successfully run

--- SET7/Set7_Q1.java
+++ SET7/Set7_Q1.java
@@ -3,7 +3,9 @@ package SET7;
 // Write a program to Print number from 1 to n.
 public class Set7_Q1 {
     public static void main(String args[]) {
-        Integer num = 0;
-        System.out.println(num);
+        int num = 0;
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
     }
 }
